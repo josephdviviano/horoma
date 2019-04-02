4e6d2cb43f033ef1ed87dbf146396e32e161ab8e
Joseph Viviano <joseph@viviano.ca>
2019-04-02 17:27:48 -0400

    added vade trainer

--- trainer/__init__.py
+++ trainer/__init__.py
@@ -2,3 +2,4 @@ from .basetrainer import BaseTrainer
 from .kmeans_trainer import KMeansTrainer
 from .cluster_trainer import ClusterTrainer
 from .cluster_kmeans_trainer import ClusterKMeansTrainer
+from .cluster_kmeans_vade_trainer import ClusterKMeansVadeTrainer
