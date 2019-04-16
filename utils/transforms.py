abc1f042b541ce6b50f306cc1f8e1803d981d0d2
Joseph Viviano <joseph@viviano.ca>
2019-04-15 20:43:11 -0400

    added resnet friendly transforms

--- utils/transforms.py
+++ utils/transforms.py
@@ -20,6 +20,26 @@ class RandomQuarterTurn:
         return functional.rotate(img, angle)
 
 
+class HoromaTransformsResNet:
+    """
+    Performs all transforms at once for resnet.
+    """
+
+    def __init__(self):
+        self.transforms = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.Resize((224, 224)),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomVerticalFlip(),
+            RandomQuarterTurn(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.56121268, 0.20801756, 0.2602411], std=[0.22911494, 0.10410614, 0.11500103])
+        ])
+
+    def __call__(self, img):
+        return self.transforms(img)
+
+
 class HoromaTransforms:
     """
     Performs all transforms at once.
