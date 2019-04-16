2288c55d110f9d86bc5c0f1bf2a1d7c2da0cde65
Joseph Viviano <joseph@viviano.ca>
2019-04-15 20:42:15 -0400

    added resnet stuff

--- models/__init__.py
+++ models/__init__.py
@@ -4,3 +4,4 @@ from .wasserstein import WassersteinAutoEncoder
 from .simple_ae import SimpleAutoEncoder
 from .variational import VariationalAutoEncoder
 from .conv_ae import ConvolutionalAutoEncoder
+from .resnet import ResNet18
