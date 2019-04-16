81a4f48722dad4649073c8c3e660eff88b1a1e77
Joseph Viviano <joseph@viviano.ca>
2019-04-15 20:42:31 -0400

    added resnet stuff

--- trainer/__init__.py
+++ trainer/__init__.py
@@ -1,4 +1,6 @@
 from .basetrainer import BaseTrainer
+from .trainer import Trainer
+from .supervised_trainer import SupervisedTrainer
 from .kmeans_trainer import KMeansTrainer
 from .cluster_trainer import ClusterTrainer
 from .cluster_kmeans_trainer import ClusterKMeansTrainer
