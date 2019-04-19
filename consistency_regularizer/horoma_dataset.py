90251d0ea77c48205cf9c66a0b47907a314068d7
Vicki Anand <vicki_anand@hotmail.com>
2019-04-19 12:50:21 -0400

    Add a constant map_labels dict for mapping labels to integer

--- consistency_regularizer/horoma_dataset.py
+++ consistency_regularizer/horoma_dataset.py
@@ -86,10 +86,10 @@ class HoromaDataset(Dataset):
             else:
                 pre_targets = pre_targets[skip : skip + subset]
 
-            self.map_labels = np.unique(pre_targets)
-
+            self.map_labels = {'BJ': 0, 'BP': 1, 'CR': 2, 'EB': 3, 'EN': 4, 'EO': 5, 'ES': 6, 'EU': 7, 'FR': 8, 'HG': 9, 'PB': 10, 'PE': 11, 'PR': 12, 'PT': 13, 'PU': 14, 'SB': 15, 'TO': 16}
+            # print(f"for {filename_y}, map_labels: \n{self.map_labels}")
             self.targets = np.asarray(
-                [np.where(self.map_labels == t)[0][0] for t in pre_targets]
+                [self.map_labels[k] for k in pre_targets]
             )
         self.labeled = (self.targets is not None)
 
